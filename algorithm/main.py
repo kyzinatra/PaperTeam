e484de59f7167c406b9dabd5b1d6e4d53d346179
Nekttuman <nekttuman@gmail.com>
2022-05-02 03:07:41 +0300

    Pattern and TargetPattern implementation done
    folding simulation works, template square is calculated

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,32 +1,33 @@
 from modules.Patterns import Pattern, TargetPattern
+import json
+         
 
-    
-from copy import deepcopy
-def getInvertByAxis(tf,ax):
-    resF = deepcopy(tf)
-    ###############################
-    return resF      
+def resolve_example(ptrn):
+    tp = TargetPattern(ptrn.h_size, ptrn.v_size)
+    tp.setPaddings(1, 0.5, 1, 0.5)
+    tp.visualizate()
+    print(tp.square)
+    tp.foldByAx('E')
+    tp.visualizate()
+    print(tp.square)
+    tp.foldByAx('H')
+    tp.visualizate()
+    print(tp.square)
+    tp.foldByAx('B')
+    tp.visualizate()
+    print(tp.square)
 
+    tp.foldByAx('G')
+    tp.visualizate()
+    print(tp.square)
+    tp.foldByAx('F')
+    tp.visualizate()
+    print(tp.square)
+    tp.foldByAx('A')
+    tp.visualizate()
+    print(tp.square)
 
-f = Pattern()
-
-f.test_init()
-h = 3
-v = 6
-f.prnt()
-f.visualizate()
-
-
-'''
-3
-1010101
-0011100
-0101010
-6
-0011
-1111
-1100
-1100
-1111
-0011
-'''
+if __name__ == '__main__':
+    f = open("test_init.json")
+    ptrn = Pattern(json.load(f))
+    resolve_example(ptrn)
--- algorithm/modules/Patterns.py
+++ algorithm/modules/Patterns.py
@@ -1,11 +1,25 @@
-from regex import P
+from modules.Links import Links, Segment
 
 
 class Pattern:
     def __init__(self, js_obj=None):
         if (js_obj):
-            self.horiz = js_obj['horiz']
-            self.vert = js_obj['vert']
+            self.horiz = {}
+            for str_ax in js_obj['horiz']:
+                seg_ax = []
+                pos = 0
+                for seg_state in js_obj['horiz'][str_ax]:
+                    seg_ax.append(Segment(int(seg_state), str_ax, pos))
+                    pos += 1
+                self.horiz[str_ax] = seg_ax
+            self.vert = {}
+            for str_ax in js_obj['vert']:
+                seg_ax = []
+                pos = 0
+                for seg_state in js_obj['vert'][str_ax]:
+                    seg_ax.append(Segment(int(seg_state), str_ax, pos))
+                    pos += 1
+                self.vert[str_ax] = seg_ax
             self.h_size = len(self.horiz)
             self.v_size = len(self.vert)
             self.padding = js_obj['padding']
@@ -14,262 +28,220 @@ class Pattern:
             self.horiz = {}
             self.h_size = 0
             self.v_size = 0
-            self.padding = {"top": 1, "right": 1, "bottom": 1, "left": 1}
+            self.padding = {'top': 1, 'right': 1, 'bottom': 1, 'left': 1}
 
     def visualizate(self):
         line_c = 0
         print('  ', end='')
         for n in self.vert.keys():
-            print("   "+n, end='')
+            print('   '+n, end='')
         print('\n  ', end='')
         for i in self.horiz.keys():
             for j in self.vert.values():
-                print("   " + ('┃' if int(j[line_c]) else '┆'), end='')
+                print('   ' + ('┃' if j[line_c].state else '┆'), end='')
             line_c += 1
             print('\n '+i, end='')
             for k in self.horiz[i]:
-                print('───+' if int(k) else '┅┅┅+', end='')
+                print('───+' if k.state else '┅┅┅+', end='')
             print('\n  ', end='')
         for j in self.vert.values():
-            print("   " + ('┃' if int(j[line_c]) else '┆'), end='')
+            print('   ' + ('┃' if j[line_c].state else '┆'), end='')
         print('')
 
-    def inputPattern(self):
-        Pattern.h_size = int(input("h_size = "))
-        tmp_ax = 0
-        for i in range(Pattern.h_size):
-            tmp_ax = input(chr(i+65) + " ax = ")
-            self.horiz[chr(i+65)] = tmp_ax
-        self.v_size = int(input("v_size = "))
-        for i in range(self.h_size, self.h_size+self.v_size):
-            tmp_ax = input(chr(i+65) + " ax = ")
-            self.vert[chr(i+65)] = tmp_ax
-        self.padding["top"] = int(input("margins 0-1 values\ntop = "))
-        self.padding["right"] = int(input("right = "))
-        self.padding["bottom"] = int(input("bottom = "))
-        self.padding["left"] = int(input("left = "))
-
-    def test_init(self):
-        test_d = ['1010101', '0011100', '0101010', '0011',
-                  '1111', '1100', '1100', '1111', '0011']
-        self.h_size = 3
-        tmp_ax = 0
-        for i in range(self.h_size):
-            tmp_ax = test_d[i]
-            self.horiz[chr(i+65)] = tmp_ax
-        self.v_size = 6
-        for i in range(self.h_size, self.h_size+self.v_size):
-            tmp_ax = test_d[i]
-            self.vert[chr(i+65)] = tmp_ax
-        self.padding = {"top": 0, "right": 0.5, "bottom": 0.5, "left": 0}
-
     def prnt(self):
-        print("horiz:", self.horiz, "\nVert: ",
-              self.vert, "\npaddings: ", self.padding)
-
-    def findWholeAx(self):
-        ans = []
-        for k in self.vert.keys():
-            if self.vert[k] == '1'*(self.h_size+1) or self.vert[k] == '0'*(self.h_size+1):
-                ans.append(k)
-        return ans
-
-
+        print('horiz:', self.horiz, '\nVert: ',
+              self.vert, '\npaddings: ', self.padding)
 
 
 class TargetPattern(Pattern):
     def __init__(self, h_size, v_size):
         Pattern.__init__(self)
-        self.linked_axis = []
+        self.links = Links()
         self.h_size = h_size
         self.v_size = v_size
-        self.square = (v_size + self.padding["top"] + self.padding["bottom"])*(
-            h_size + self.padding["right"] + self.padding["left"])
-        for i in range(self.h_size):
-            self.horiz[chr(i+65)] = '0'*(v_size+1)
+        self.valid_axis_vert = []
+        self.valid_axis_horiz = []
+        self.square = (v_size + -1)*(h_size - 1) + (self.padding['left'])*(h_size-1) + (self.padding['right'])*(
+            h_size-1) + (self.padding['top'])*(v_size-1) + (self.padding['bottom'])*(v_size-1)
+        for i in range(0, self.h_size):
+            h_ax = []
+            self.valid_axis_horiz.append(chr(i+65))
+            for j in range(0, self.v_size+1):
+                h_ax.append(Segment(0, chr(i+65), j))
+            self.horiz[chr(i+65)] = h_ax
         for i in range(self.h_size, self.h_size+self.v_size):
-            self.vert[chr(i+65)] = '0'*(h_size+1)
+            v_ax = []
+            self.valid_axis_vert.append(chr(i+65))
+            for j in range(0, self.h_size+1):
+                v_ax.append(Segment(0, chr(i+65), j))
+            self.vert[chr(i+65)] = v_ax
 
-    def setLink(self, ax1, ax2):
-        for i in range(0, len(self.linked_axis)):
-            if ax1 in self.linked_axis[i]:
-                self.linked_axis[i].append(ax2)
-                return
-            elif ax2 in self.linked_axis[i]:
-                self.linked_axis[i].append(ax1)
-                return
+    def setPaddings(self, top, right, bottom, left):
+        self.padding['top'] = top
+        self.padding['right'] = right
+        self.padding['bottom'] = bottom
+        self.padding['left'] = left
+        self.__updateSquare()
+
+    def markAsFold(self, ax):
+        if ax in self.vert:
+            for seg in self.vert[ax]:
+                self.links.markAsFold(seg)
+                seg.markAsFold()
+        elif ax in self.horiz:
+            for seg in self.horiz[ax]:
+                self.links.markAsFold(seg)
+                seg.markAsFold()
         else:
-            self.linked_axis.append([ax1, ax2])
+            raise Exception('template does not have such an axis')
 
-    def setLinks(self, ax):
-        if ax in self.vert:
-            i = 1
-            while chr(ord(ax)-i) in self.vert and chr(ord(ax)+i) in self.vert:
-                self.setLink(chr(ord(ax)-i), chr(ord(ax)+i))
-                i += 1
-        elif ax in self.horiz:
-            i = 1
-            while chr(ord(ax)-i) in self.horiz and chr(ord(ax)+i) in self.horiz:
-                self.setLink(chr(ord(ax)-i), chr(ord(ax)+i))
-                i += 1
+    def isAxisInEndHalf(self, ax):
+        if ax in self.valid_axis_vert:
+            return (((ord(max(self.valid_axis_vert))-ord(min(self.valid_axis_vert)))/2
+                    + ord(min(self.valid_axis_vert)) - ord(ax)) <= 0)
+        elif ax in self.valid_axis_horiz:
+            return (((ord(max(self.valid_axis_horiz))-ord(min(self.valid_axis_horiz)))/2
+                    + ord(min(self.valid_axis_horiz)) - ord(ax)) <= 0)
 
-    def axisInEndHalf(self, ax):
+    def __getAxNumber(self, ax):
+        count = 0
         if ax in self.vert:
-            return (((ord(max(self.vert))-ord(min(self.vert)))/2 + ord(min(self.vert)) - ord(ax)) <= 0)
-        elif ax in self.horiz:
-            return (((ord(max(self.horiz))-ord(min(self.horiz)))/2 + ord(min(self.horiz))- ord(ax)) <= 0)
-    
-    def get_inverted_ax(self, ax):
-        res = ""
-        if ax in self.vert:
-            if self.vert[ax][0] == '-':
-                return self.vert[ax]
-            for i in self.vert[ax]:
-                res += "0" if (i == "1") else "1"
-        elif ax in self.horiz:
-            if self.horiz[ax][0] == '-':
-                return self.vert[ax]
-            for i in self.horiz[ax]:
-                res += "0" if (i == "1") else "1"
-        return res
-    
-    def getSplittedAxisBy(self, ax, sep_ax):
-        def countAxNumber(ax):
-            count = 0
-            if ax in self.vert:
-                for i in self.vert.keys():
-                    if i == ax:
-                        return count
-                    count += 1
-            if ax in self.horiz:
-                for i in self.horiz.keys():
-                    if i == ax:
-                        return count
-                    count += 1
-            return -1
+            for i in self.vert:
+                if i == ax:
+                    return count
+                count += 1
+        else:
+            for i in self.horiz:
+                if i == ax:
+                    return count
+                count += 1
 
-        direct = (1 if self.axisInEndHalf(sep_ax) else -1)
-        if ax in self.vert:
-            if self.vert[ax][0] == '-':
-                return self.vert[ax]
-            n = countAxNumber(sep_ax) + (1 if direct == -1 else 0)
-            i = 1
-            res = [i for i in self.vert[ax]]
-            while n+i*direct < len(self.vert[ax]) and n+i*direct >= 0:
-                res[n+i*direct] = '0' if res[n-i*direct] == '1' else '1'
-                i+=1
+    def __getAxNumberInValid(self, ax):
+        count = 0
+        if ax in self.valid_axis_vert:
+            for i in self.valid_axis_vert:
+                if i == ax:
+                    return count
+                count += 1
+        else:
+            for i in self.valid_axis_horiz:
+                if i == ax:
+                    return count
+                count += 1
+
+    def __updatePaddings(self, ax):
         if ax in self.horiz:
-            if self.horiz[ax][0] == '-':
-                return self.horiz[ax]
-            n = countAxNumber(sep_ax) + (1 if direct == -1 else 0)
-            i = 1
-            res = [i for i in self.horiz[ax]]
-            while n+i*direct < len(self.horiz[ax]) and n+i*direct >= 0:
-                res[n+i*direct] = '0' if res[n-i*direct] == '1' else '1'
-                i+=1
-        return ''.join(res)
+            direct = ('top' if not self.isAxisInEndHalf(ax) else 'bottom')
+        else:
+            direct = ('left' if not self.isAxisInEndHalf(ax) else 'right')
+        self.padding[direct] = 1
 
-
-
-    def invertLinked(self, inv_ax, trgt):
-        for i in self.linked_axis:
-            if inv_ax in i:
-                for j in range(0,len(i)):
-                    if i[j] in self.vert:
-                        self.vert[i[j]] = self.get_inverted_ax(trgt) if j % 2 == 0 else self.vert[trgt]
-                    elif i[j] in self.horiz:
-                        self.horiz[i[j]] = self.get_inverted_ax(trgt) if j % 2 == 0 else self.vert[trgt]
-
-    def isFold(self, ax):
-        if ax in self.vert:
-            return self.vert[ax][0] == '-'
-        elif ax in self.horiz:
-            return self.horiz[ax][0] == '-'
-        raise Exception("template does not have such an axis")
-        
-
-    def markAsFold(self,ax):
-        if ax in self.vert:
-            self.vert[ax] = '-'*(self.h_size+1)
-            for links in self.linked_axis:
-                if ax in links:
-                    for l in links:
-                        self.vert[l] = '-'*(self.h_size+1)
-        elif ax in self.horiz:
-            self.horiz[ax] = '-'*(self.v_size+1)
-            for links in self.linked_axis:
-                if ax in links:
-                    for l in links:
-                        self.horiz[l] = '-'*(self.v_size+1)
-        raise Exception("template does not have such an axis")
+    def __updateSquare(self):
+        v_size = len(self.valid_axis_vert)
+        h_size = len(self.valid_axis_horiz)
+        self.square = (v_size - 1)*(h_size - 1) + (self.padding['left'])*(h_size-1) + (self.padding['right'])*(
+            h_size-1) + (self.padding['top'])*(v_size-1) + (self.padding['bottom'])*(v_size-1) + (
+            self.padding['right'] + self.padding['left'])*(self.padding['top']+self.padding['bottom'])
 
     def foldByAx(self, ax):
-        if ax in self.vert:
-            direct = (1 if self.axisInEndHalf(ax) else -1)
-            i = 1
-            while chr(ord(ax)+i*direct) in self.vert:
-                inv_ax = chr(ord(ax)+i*direct)
-                trgt_ax = chr(ord(ax)-i*direct)
-                if not self.isFold(inv_ax):
-                    self.vert[inv_ax] = self.get_inverted_ax(trgt_ax)
-                    self.invertLinked(inv_ax, trgt_ax)
+        self.__updatePaddings(ax)
+        if ax in self.valid_axis_vert:
+            self.foldByVertAx(ax)
+        elif ax in self.valid_axis_horiz:
+            self.foldByHorizAx(ax)
+        self.__updateSquare()
 
-                i+=1
-            for j in self.horiz.keys():
-                self.horiz[j] = self.getSplittedAxisBy(j, ax)
-        elif ax in self.horiz:
-            direct = (1 if self.axisInEndHalf(ax) else -1)
-            i = 1
-            while chr(ord(ax)+i*direct) in self.horiz:
-                inv_ax = chr(ord(ax)+i*direct)
-                trgt_ax = chr(ord(ax)-i*direct)
-                if not self.isFold(inv_ax):
-                    self.horiz[inv_ax] = self.get_inverted_ax(trgt_ax)
-                    self.invertLinked(inv_ax, trgt_ax)
+    def foldByVertAx(self, ax):
+        direct = (1 if self.isAxisInEndHalf(ax) else -1)
+        # set links for vert axis
+        depend_ax = chr(ord(ax)+direct)
+        target_ax = chr(ord(ax)-direct)
+        while depend_ax in self.valid_axis_vert:
+            for i in range(len(self.vert[depend_ax])):
+                self.links.setLink(
+                    self.vert[depend_ax][i], self.vert[target_ax][i])
+            depend_ax = chr(ord(depend_ax) + direct)
+            target_ax = chr(ord(target_ax) - direct)
+        # set links for horiz axis
+        depend_segN = self.__getAxNumber(ax) + (1 if direct == 1 else 0)
+        target_segN = self.__getAxNumber(ax) + (1 if direct == -1 else 0)
+        diff = self.__getAxNumber(ax)-self.__getAxNumberInValid(ax)
+        while depend_segN <= diff+len(self.valid_axis_vert) and depend_segN >= diff:
+            for hAx in self.valid_axis_horiz:
+                self.links.setLink(
+                    self.horiz[hAx][depend_segN], self.horiz[hAx][target_segN])
+            depend_segN += direct
+            target_segN -= direct
+        # remove depend from valid_axis +
+        depend_ax = chr(ord(ax))
+        while depend_ax in self.valid_axis_vert:
+            self.valid_axis_vert.remove(depend_ax)
+            depend_ax = chr(ord(depend_ax) + direct)
+        self.markAsFold(ax)
 
-                i+=1
-            for j in self.vert.keys():
-                self.vert[j] = self.getSplittedAxisBy(j, ax)
-
-        self.setLinks(ax)
+    def foldByHorizAx(self, ax):
+        direct = (1 if self.isAxisInEndHalf(ax) else -1)
+        # set links for horiz axis
+        depend_ax = chr(ord(ax)+direct)
+        target_ax = chr(ord(ax)-direct)
+        while depend_ax in self.valid_axis_horiz:
+            for i in range(len(self.horiz[depend_ax])):
+                self.links.setLink(
+                    self.horiz[depend_ax][i], self.horiz[target_ax][i])
+            depend_ax = chr(ord(depend_ax) + direct)
+            target_ax = chr(ord(target_ax) - direct)
+        # set links for vert axis
+        depend_segN = self.__getAxNumber(ax) + (1 if direct == 1 else 0)
+        target_segN = self.__getAxNumber(ax) + (1 if direct == -1 else 0)
+        diff = self.__getAxNumber(ax)-self.__getAxNumberInValid(ax)
+        while depend_segN <= diff+len(self.valid_axis_horiz) and depend_segN >= diff:
+            for hAx in self.valid_axis_vert:
+                self.links.setLink(
+                    self.vert[hAx][depend_segN], self.vert[hAx][target_segN])
+            depend_segN += direct
+            target_segN -= direct
+        # remove depend from valid_axis +
+        depend_ax = chr(ord(ax))
+        while depend_ax in self.valid_axis_horiz:
+            self.valid_axis_horiz.remove(depend_ax)
+            depend_ax = chr(ord(depend_ax) + direct)
         self.markAsFold(ax)
 
     def visualizate(self):
         line_c = 0
         print('  ', end='')
         for n in self.vert.keys():
-            print("   "+n, end='')
+            print('   '+n, end='')
         print('\n  ', end='')
         for i in self.horiz.keys():
             for j in self.vert.values():
-                if j[line_c] == '1':
-                    symb = '┃'
-                elif j[line_c] == '0':
-                    symb = '┆'
+                if j[line_c].isFold:
+                    line = '\\'
+                elif j[line_c].state:
+                    line = '┃'
                 else:
-                    symb = '\\'
-                print("   " + symb, end='')
+                    line = '┆'
+                print('   ' + line, end='')
             line_c += 1
             print('\n '+i, end='')
             for k in self.horiz[i]:
-                if k == '1':
-                    symb = '───+'
-                elif k == '0':
-                    symb = '┅┅┅+'
+                if k.isFold:
+                    line = '\\\\\+'
+                elif k.state:
+                    line = '───+'
                 else:
-                    symb = '\\\\\+'
-                print(symb, end='')
+                    line = '┅┅┅+'
+                print(line, end='')
             print('\n  ', end='')
         for j in self.vert.values():
-            if j[line_c] == '1':
-                symb = '┃'
-            elif j[line_c] == '0':
-                symb = '┆'
+            if j[line_c].isFold:
+                line = '\\'
+            elif j[line_c].state:
+                line = '┃'
             else:
-                symb = '\\'
-            print("   " + symb, end='')
+                line = '┆'
+            print('   ' + line, end='')
         print('\n')
 
     def prnt(self):
         super().prnt()
-        print('linked axis: ', self.linked_axis)
