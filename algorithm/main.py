9c17848b49cf3a39ff1372d6d1217c0dbb9db930
Nekttuman <nekttuman@gmail.com>
2022-05-10 09:51:18 +0300

    add reading json from argv

--- algorithm/main.py
+++ algorithm/main.py
@@ -5,14 +5,22 @@ import sys
 
 # "json_files/test_init.json"
 
+
 if __name__ == '__main__':
-    with open(sys.argv[1]) as inp:
-        ptrn = Pattern(json.load(inp))
-    DTree = DesTree(ptrn)
-    
-    if len(sys.argv) > 2 and sys.argv[2] in ['u', 'U', '-u', '-U']:
-        bestPaths = []  # Dtree.GetUFriendlyPath()
-        print("user friendly solution output coming soon")
-    else:
+    if sys.argv[1] == '-j':
+        ptrn = Pattern(json.loads(sys.argv[2]))
+
+        DTree = DesTree(ptrn)
         bestPaths = DTree.get_best_paths()
+
+    else:
+        with open(sys.argv[1]) as inp:
+            ptrn = Pattern(json.load(inp))
+        DTree = DesTree(ptrn)
+
+        if len(sys.argv) > 2 and sys.argv[2] in ['u', 'U', '-u', '-U']:
+            bestPaths = []  # Dtree.GetUFriendlyPath()
+            print("user friendly solution output coming soon")
+        else:
+            bestPaths = DTree.get_best_paths()
     print(bestPaths, sep='\n')
