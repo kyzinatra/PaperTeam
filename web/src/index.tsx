b9462bfd86d813f481d952e6972c77c1386314bc
kyzinatra <main@kyzinatra.ru>
2022-05-13 11:22:52 +0300

    heroku push

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -7,8 +7,10 @@ import "./sass/global.sass";
 import { store } from "./service/redux/store";
 import { Provider } from "react-redux";
 console.log(process.env.NODE_ENV);
+
 const el = document.getElementById("root") as Element;
 const root = createRoot(el);
+
 root.render(
   <Provider store={store}>
     <App />
