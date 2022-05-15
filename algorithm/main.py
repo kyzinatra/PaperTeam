0a79ec3e447c2a30c3917c58f324894f7b370e6f
Nekttuman <nekttuman@gmail.com>
2022-05-15 16:05:38 +0300

    rename DesTree to SolutionTree

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,5 +1,5 @@
 from modules.Patterns import Pattern
-from modules.DesTree import DesTree
+from modules.SolutionTree import SolutionTree
 import json
 import sys
 import timeit
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     if sys.argv[1] == "-j":
         ptrn = Pattern(json.loads(sys.argv[2]))
 
-        DTree = DesTree(ptrn)
+        DTree = SolutionTree(ptrn)
         bestPaths = DTree.get_best_paths()
 
     else:
         with open(sys.argv[1]) as inp:
             ptrn = Pattern(json.load(inp))
-        DTree = DesTree(ptrn)
+        DTree = SolutionTree(ptrn)
         bestPaths = DTree.get_best_paths(1)
 
     print(json.dumps(bestPaths), sep="\n")
