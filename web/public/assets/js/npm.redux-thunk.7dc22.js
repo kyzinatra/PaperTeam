59ab4e92d18347779b753eca55b535e7eb018cf8
Danil <danilsolom@mail.ru>
2023-08-06 14:55:20 +1000

    npm audit fix

--- web/public/assets/js/npm.redux-thunk.7dc22.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkweb=self.webpackChunkweb||[]).push([[623],{3894:(t,n,e)=>{function u(t){return function(n){var e=n.dispatch,u=n.getState;return function(n){return function(r){return"function"==typeof r?r(e,u,t):n(r)}}}}e.d(n,{Z:()=>c});var r=u();r.withExtraArgument=u;const c=r}}]);
